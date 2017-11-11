1be95354f292ff2b2ac56f4ffa0a2a98c5a593cc
Rutuja Faldu <rutujafaldu@rutujas-mbp.mobile.asu.edu>
2017-11-11 15:52:59 -0700

    indentation fixes

--- shortest_path.py
+++ shortest_path.py
@@ -4,7 +4,7 @@ import FindMST
 
 #############################################
 class MarkedPath:
-    
+
     def __init__(self,source,cost,path):
         self.source = source
         self.cost = cost
@@ -23,7 +23,7 @@ def addMarkedPath(markedPaths, id, source, cost, path):
         value = []
         value.append(markedPathObj)
         markedPaths[id] = value
-#print(id, markedPathObj.source, markedPathObj.cost, markedPathObj.path)
+        #print(id, markedPathObj.source, markedPathObj.cost, markedPathObj.path)
 
 #################################################################################
 
@@ -47,28 +47,28 @@ def findMST(markedPaths, markedSet, graphDB):
             print("\nUnique values of Key "+markedSet[i]+": ")
             for u in uniquePathObjects:
                 print(u.source, u.cost, u.path)
-            
+
             #storing the cost in SP matrix
             for j in range(0,uniquePathObjects.__len__()):
                 ind = markedSet.index(uniquePathObjects[j].source)
                 SP[ind][i] = uniquePathObjects[j].cost
 
-print("\n\n")
-print("SP matrix: \n")
-for i in range(0,k+1):
-    print(SP[i])
-    
+    print("\n\n")
+    print("SP matrix: \n")
+    for i in range(0,k+1):
+        print(SP[i])
+
     g = FindMST.Graph(k+1)
     for i in range(0,k+1):
         for j in range(0,k+1):
             if SP[j][i] > 0:
-                g.addEdge(i,j,SP[j][i])
+               g.addEdge(i,j,SP[j][i])
 
-result = g.KruskalMST()
-TreeMIN = [[0 for j in range(k+1)] for i in range(k+1)]
-for u,v,weight in result:
-    TreeMIN[u][v] = weight
-    
+    result = g.KruskalMST()
+    TreeMIN = [[0 for j in range(k+1)] for i in range(k+1)]
+    for u,v,weight in result:
+        TreeMIN[u][v] = weight
+
     print("\n\n")
     print("MST matrix: \n")
     for i in range(0,k+1):
@@ -81,7 +81,7 @@ def main():
     graphDb = dbWriter.DirectedGraph("bolt://127.0.0.1:7687")
     nodes = graphDb.getAllNodes()
     #graphDb.clearGraph();
-    
+
     terminals = ['14','15','20','23','25','27','29','38'];
     lengths = [0]* nodes.__len__()
     for i in graphDb.getAllNodes():
@@ -90,16 +90,16 @@ def main():
     maxLength = math.log((nodes.__len__()),2)
     markedPaths = dict()
     for i in range(0,terminals.__len__()):
-        
+
         hops = 0
         seed = terminals[i]
         costSeed = lengths[int(seed)-1]
         hops = hops + costSeed
-        
+
         ids = []
         costs = []
         paths = []
-        
+
         neighs = graphDb.getNeighboringNodes(seed)
         for j in range(0,neighs.__len__()):
             path = [seed,neighs[j]]
@@ -112,18 +112,18 @@ def main():
         costs[:] = [x - minCost for x in costs]
         hops = hops + minCost
 
-while(hops <= maxLength):
-    ind = [index for index, value in enumerate(costs) if value == 0]
-    for j in range(0,ind.__len__()):
-        expandNode = ids[ind[j]]
-        neighs = graphDb.getNeighboringNodes(expandNode)
-        
-        for k in range(0,neighs.__len__()):
-            path = paths[ind[j]]
-            
-            temp = [index for index, value in enumerate(path) if value == neighs[k]]
-                if(temp.__len__()>0):
-                    continue
+        while(hops <= maxLength):
+            ind = [index for index, value in enumerate(costs) if value == 0]
+            for j in range(0,ind.__len__()):
+                expandNode = ids[ind[j]]
+                neighs = graphDb.getNeighboringNodes(expandNode)
+
+                for k in range(0,neighs.__len__()):
+                    path = paths[ind[j]]
+
+                    temp = [index for index, value in enumerate(path) if value == neighs[k]]
+                    if(temp.__len__()>0):
+                        continue
                     else:
                         path1 = []
                         for p in range(0, path.__len__()):
@@ -134,26 +134,26 @@ while(hops <= maxLength):
                         costs.append(lengths[int(neighs[k])-1])
                         paths.append(path)
                         addMarkedPath(markedPaths,neighs[k],seed,hops,path)
-    
-        ids = [z for x,z in enumerate(ids) if x not in ind]
+
+            ids = [z for x,z in enumerate(ids) if x not in ind]
             costs = [z for x,z in enumerate(costs) if x not in ind]
             paths = [z for x,z in enumerate(paths) if x not in ind]
-            
+
             minCost = min(costs)
             costs[:] = [x - minCost for x in costs]
             hops = hops + minCost
 
-print("Marked Paths:")
-for k,v in markedPaths.items():
-    print("Key:" + k)
-    for v1 in v:
-        print(v1.source, v1.cost, v1.path)
+    print("Marked Paths:")
+    for k,v in markedPaths.items():
+        print("Key:" + k)
+        for v1 in v:
+            print(v1.source, v1.cost, v1.path)
 
     print("\n")
 
-findMST(markedPaths,terminals,graphDb)
+    findMST(markedPaths,terminals,graphDb)
 
-#############################################################################
+    #############################################################################
 
 if __name__ == "__main__":
     main()
